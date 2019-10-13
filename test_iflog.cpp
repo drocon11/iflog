518cc294a083bb4f682539d3f165742b0c2a630a
Yukinori Yamazoe <drocon11@gmail.com>
2019-10-13 21:48:57 +0900

    update

--- test_iflog.cpp
+++ test_iflog.cpp
@@ -43,7 +43,7 @@ class Runner
 public:
     int run()
     {
-        AAA aaa = IFLOG_MOVE(AAA());
+        AAA aaa = IFLOG(AAA());
         if (!IFLOG(aaa.send()))
         {
             return -1;
@@ -76,7 +76,7 @@ void test_iflog()
     assert(g_oss.str() == "LOG3:test_iflog.cpp:test_iflog | a => v , a => v , a => v , a => v , a => v , a => v , a => v , a => v , a => v , a => v , a => v\n");
     g_oss.str("");
 
-    AAA aaa = IFLOG_MOVE(AAA());
+    AAA aaa = IFLOG(AAA());
     assert(g_oss.str() == "LOG3:test_iflog.cpp:test_iflog | AAA()\n");
     g_oss.str("");
 
@@ -94,32 +94,21 @@ void test_iflog()
     iserror = IFLOG(is_error(), 1, 2, 3, 4, 5, 6, 7, 8, 9);
     iserror = IFLOG(is_error(), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+    int abc = 789;
+    int def = IFLOG(static_cast<int&&>(abc));
     std::unique_ptr<hoge> p0(IFLOG(new hoge()));
-#ifndef IFLOG_DISABLE_LOG
-    std::unique_ptr<hoge> p1  = IFLOG_MOVE(p0);
-    std::unique_ptr<hoge> p2  = IFLOG_MOVE(p1 , 1);
-    std::unique_ptr<hoge> p3  = IFLOG_MOVE(p2 , 1, 2);
-    std::unique_ptr<hoge> p4  = IFLOG_MOVE(p3 , 1, 2, 3);
-    std::unique_ptr<hoge> p5  = IFLOG_MOVE(p4 , 1, 2, 3, 4);
-    std::unique_ptr<hoge> p6  = IFLOG_MOVE(p5 , 1, 2, 3, 4, 5);
-    std::unique_ptr<hoge> p7  = IFLOG_MOVE(p6 , 1, 2, 3, 4, 5, 6);
-    std::unique_ptr<hoge> p8  = IFLOG_MOVE(p7 , 1, 2, 3, 4, 5, 6, 7);
-    std::unique_ptr<hoge> p9  = IFLOG_MOVE(p8 , 1, 2, 3, 4, 5, 6, 7, 8);
-    std::unique_ptr<hoge> p10 = IFLOG_MOVE(p9 , 1, 2, 3, 4, 5, 6, 7, 8, 9);
-    std::unique_ptr<hoge> p   = IFLOG_MOVE(p10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-#endif
 
-    IFLOG_VOID(void_func());
-    IFLOG_VOID(void_func(), 1);
-    IFLOG_VOID(void_func(), 1, 2);
-    IFLOG_VOID(void_func(), 1, 2, 3);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5, 6);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5, 6, 7);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5, 6, 7, 8);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5, 6, 7, 8, 9);
-    IFLOG_VOID(void_func(), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    IFLOG(void_func());
+    IFLOG(void_func(), 1);
+    IFLOG(void_func(), 1, 2);
+    IFLOG(void_func(), 1, 2, 3);
+    IFLOG(void_func(), 1, 2, 3, 4);
+    IFLOG(void_func(), 1, 2, 3, 4, 5);
+    IFLOG(void_func(), 1, 2, 3, 4, 5, 6);
+    IFLOG(void_func(), 1, 2, 3, 4, 5, 6, 7);
+    IFLOG(void_func(), 1, 2, 3, 4, 5, 6, 7, 8);
+    IFLOG(void_func(), 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    IFLOG(void_func(), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     iserror = IFLOG2(is_error());
     iserror = IFLOG2(is_error(), 1);
@@ -135,7 +124,7 @@ void test_iflog()
 
     Runner* runner = IFLOG(new Runner());
     IFLOG(runner->run());
-    IFLOG_VOID(delete runner);
+    IFLOG(delete runner);
 
 //    std::cout << g_oss.str();
 }
